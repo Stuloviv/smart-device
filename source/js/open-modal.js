be465f7c3b7dd28413da2e84740b31b6415556fe
Stuloviv <stuloviv@gmail.com>
2023-03-11 21:04:54 +0400

    Added focus-lock on modal window

--- source/js/open-modal.js
+++ source/js/open-modal.js
@@ -1,15 +1,19 @@
+import {FocusLock} from './utils/focus-lock';
+
 const modalContainerElement = document.querySelector('.modal-container');
 const headerCallbackElement = document.querySelector('.header__callback-button');
 const closeButtonElement = modalContainerElement.querySelector('.modal__close-button');
 const modalNameFieldElement = modalContainerElement.querySelector('.callback-form__field--name-modal');
 const pageElement = document.querySelector('.page');
 
+window.focusLock = new FocusLock();
 
 const isEscapeDown = (evt) => {
   if (evt.key === 'Escape') {
     evt.preventDefault();
     modalContainerElement.classList.add('is-close');
     pageElement.style.overflow = 'scroll';
+    window.focusLock.unlock();
   }
 };
 
@@ -17,21 +21,25 @@ const modalClose = () => {
   closeButtonElement.addEventListener('click', () => {
     modalContainerElement.classList.add('is-close');
     pageElement.style.overflow = 'scroll';
+    window.focusLock.unlock();
   });
-  document.addEventListener('keydown', isEscapeDown);
   modalContainerElement.addEventListener('click', (evt) => {
     if (evt.target.closest('.modal') === null) {
       modalContainerElement.classList.add('is-close');
       pageElement.style.overflow = 'scroll';
     }
+    window.focusLock.unlock();
   });
+  document.addEventListener('keydown', isEscapeDown);
 };
 
 if (modalContainerElement.classList.contains('is-close')) {
-  headerCallbackElement.addEventListener('click', function () {
+  headerCallbackElement.addEventListener('click', () => {
     modalContainerElement.classList.remove('is-close');
+    window.focusLock.lock('.modal');
     modalNameFieldElement.focus();
     pageElement.style.overflow = 'hidden';
     modalClose();
   });
 }
+
