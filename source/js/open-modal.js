9fa32f817337bb598ce1f5084a2751a0b90534ea
Stuloviv <stuloviv@gmail.com>
2023-03-11 05:13:17 +0400

    page layout is complete

--- source/js/open-modal.js
+++ source/js/open-modal.js
@@ -2,13 +2,36 @@ const modalContainerElement = document.querySelector('.modal-container');
 const headerCallbackElement = document.querySelector('.header__callback-button');
 const closeButtonElement = modalContainerElement.querySelector('.modal__close-button');
 const modalNameFieldElement = modalContainerElement.querySelector('.callback-form__field--name-modal');
+const pageElement = document.querySelector('.page');
+
+
+const isEscapeDown = (evt) => {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    modalContainerElement.classList.add('is-close');
+    pageElement.style.overflow = 'scroll';
+  }
+};
+
+const modalClose = () => {
+  closeButtonElement.addEventListener('click', () => {
+    modalContainerElement.classList.add('is-close');
+    pageElement.style.overflow = 'scroll';
+  });
+  document.addEventListener('keydown', isEscapeDown);
+  modalContainerElement.addEventListener('click', (evt) => {
+    if (evt.target.closest('.modal') === null) {
+      modalContainerElement.classList.add('is-close');
+      pageElement.style.overflow = 'scroll';
+    }
+  });
+};
 
 if (modalContainerElement.classList.contains('is-close')) {
   headerCallbackElement.addEventListener('click', function () {
     modalContainerElement.classList.remove('is-close');
     modalNameFieldElement.focus();
-    closeButtonElement.addEventListener('click', function () {
-      modalContainerElement.classList.add('is-close');
-    });
+    pageElement.style.overflow = 'hidden';
+    modalClose();
   });
 }
